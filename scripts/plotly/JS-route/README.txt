a90b870f7cfc1d4c69f657eb14e92563ba2e578f
Louwrens Labuschagne <louwjlabuschagne@gmail.com>
2017-08-28 15:49:40 +0200

    changing to using plotly with JS instead of R and compiling a html to use, also cleaned up the scripts directory a bit

---
+++ scripts/plotly/JS-route/README.txt
@@ -0,0 +1,3 @@
+A Pen created at CodePen.io. You can find this one at https://codepen.io/louwjlab/pen/prxwGo.
+
+ World Choropleth Map - Robinson Projection 2010 with Plotly.js.
--- scripts/plotly/plotly.R
+++ scripts/plotly/plotly.R
@@ -1,13 +1,21 @@
 
 
 library(plotly)
-user_name = "louwjlabuschagne"
-api_key = ""
+# user_name = "louwjlabuschagne"
+# api_key = ""
 
-Sys.setenv("plotly_username"=user_name)
-Sys.setenv("plotly_api_key"=api_key)
+# Sys.setenv("plotly_username"=user_name)
+# Sys.setenv("plotly_api_key"=api_key)
 
 df <- read.csv('https://raw.githubusercontent.com/plotly/datasets/master/2014_world_gdp_with_codes.csv')
+head(df)
+library(tidyverse)
+department <- tribble(~student_id, ~age, ~country,
+                      1,   49, "Australia",
+                      2,   50, "Japan",
+                      3,   60, "United Kingdom"
+)
+df = as.data.frame(department)
 
 # light grey boundaries
 l <- list(color = toRGB("grey"), width = 0.5)
@@ -21,8 +29,8 @@ g <- list(
 
 p <- plot_geo(df) %>%
   add_trace(
-    z = ~GDP..BILLIONS., color = ~GDP..BILLIONS., colors = 'Reds',
-    text = ~COUNTRY, locations = ~CODE, marker = list(line = l)
+    z = ~age, color = ~age, colors = 'Reds',
+    text = ~country, locations = ~CODE, marker = list(line = l)
   ) %>%
   # colorbar(title = 'GDP Billions US$', tickprefix = '$') %>%
   layout(
@@ -32,7 +40,11 @@ p <- plot_geo(df) %>%
     
   ) %>%  hide_colorbar() %>% layout(title = F)
 
+htmlwidgets::saveWidget(as_widget(p)
+                        , paste0("map.html", sep="")
+                        , selfcontained=T)
+
 # Create a shareable link to your chart
 # Set up API credentials: https://plot.ly/r/getting-started
- chart_link = api_create(p, filename="9")
- chart_link
+ # chart_link = api_create(p, filename="9")
+ # chart_link
